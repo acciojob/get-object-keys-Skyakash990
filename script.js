62df8c2a6ccf50a4ba5ffc7da5305afecef953fd
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-10 17:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student={
+	name:"Akash",
+	age:"21",
+	city:"Botad"
+	function getKeys(){
+		this.name,
+		this.age,
+		this.city
+	}
+}
+console.log(student.keys())
