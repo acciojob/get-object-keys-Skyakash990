8eef706680fae6b4985180dfb3218d1589db56ad
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-10 18:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //your JS code here. If required.
 
+const student = {
+  name: "John",
+};
+
 function getKeys(obj) {
   return Object.keys(obj);
 }
- 
 
+console.log(getKeys(student)); // Output: ["name"]
+
+// Additional tests
 const student2 = { name: "John", age: 25, city: "New York" };
-console.log(getKeys(student2)); 
+console.log(getKeys(student2)); // Output: ["name", "age", "city"]
+
